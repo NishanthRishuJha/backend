43a77f2efc9aae0cf563d1b5e719ec6f14581bb0
NishanthRishuJha <nishanthrishujha@gmail.com>
2025-12-28 14:30:01 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB=async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`mongodb ho gaya connected:${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("mongodb connection error hai");
+        process.exit(1)
+    }
+}
+
+export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log(`MongoDB connected successfully: ${mongoose.connection.host}`);
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
