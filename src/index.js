3efbb5e436cc7f9feddabdf3ebef71603530c5f0
NishanthRishuJha <nishanthrishujha@gmail.com>
2025-12-29 08:16:55 +0530

    add user and video

--- src/index.js
+++ src/index.js
@@ -11,5 +11,12 @@ dotenv.config({
 })
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT ||8000,()=>{
+        console.log(`server run at ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongo db connect failed",err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//promises technique
+const asyncHandler=(requestHandler) => {
+    (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next)
+    }
+}
+
+
+
+
+
+export{asyncHandler}
+
+
+//try catch method
+// const asyncHandler=(fn)=>async(req, res, next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code ||500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
